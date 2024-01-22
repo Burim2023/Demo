ac51b09bead63806558704f2d1cc6075a7dd1393
Burim2023 <151534825+Burim2023@users.noreply.github.com>
2024-01-22 11:27:12 +0100

    Test

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,22 @@
-package PACKAGE_NAME;public class Main {
+/*
+ * Project: Logarithmische Funktion
+ * Klasse: 1 aApc
+ * Author: Burim Shala
+ * Last change:
+ *   by: Shala
+ *   date: 07.12.2023
+ * */
+
+public class Main {
+
+    public static void main(String[] args) {
+        for (int i = 80; i > 0; i--) {
+            double vergroeßerungsfaktor;
+            vergroeßerungsfaktor=85 * Math.log10(i);
+            for (int j = 0; j < vergroeßerungsfaktor; j++) {
+                System.out.print("#");
+            }
+            System.out.print("\n");
+        }
+    }
 }
