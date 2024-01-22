3dc3fd915423874a5c3e5d65aee80043ff06e1bd
Burim2023 <151534825+Burim2023@users.noreply.github.com>
2024-01-22 11:46:35 +0100

    Commit-Test

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,12 @@
-package PACKAGE_NAME;public class Main {
+public class Main {
+    public static void main(String[] args) {
+        for (int i = 80; i > 0; i--) {
+            double vergrößerungsfaktor;
+            vergrößerungsfaktor=85 * Math.log10(i);
+            for (int j = 0; j < vergrößerungsfaktor; j++) {
+                System.out.print("*");
+            }
+            System.out.print("\n");
+        }
+    }
 }
